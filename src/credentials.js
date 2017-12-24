7d3aee622a58fce430807dea213957ac48c16f66
lewis-munyi <lewismunyi97@gmail.com>
2017-12-24 21:48:12 +0300

    Added google calendar credentials. Obtain these from the google developer console

---
+++ src/credentials.js
@@ -0,0 +1,10 @@
+module.exports = {
+    api_key = "",
+    calendar_id = "",
+    client_id: "",
+    project_id: "",
+    auth_uri: "",
+    token_uri: "",
+    auth_provider_x509_cert_url: "",
+    client_secret: ""
+}
